3243ac52e24193f9bc6a36fe1e5dbcfa54b08879
sava <savamedvedev@gmail.com>
2022-10-29 19:08:32 +0800

    therd-commit

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,31 @@
 /* eslint-disable */
+import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 
 const Discover = () => {
-    const genreTitle = 'POP';
+    const dispatch = useDispatch();
+    const { genreListId } = useSelector((state) => state.player);
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'POP');
+
+    if (isFetching) {
+        return (
+            <>
+                < Loader title="Loading songs..." />
+            </>
+        )
+    }
+    if (error) {
+        return (
+            <>
+                <Error />
+            </>
+        )
+    }
+
     return (
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">

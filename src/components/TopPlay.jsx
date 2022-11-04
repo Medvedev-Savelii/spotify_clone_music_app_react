4c697ec7d816718651934aa1a9b5bc1225c39e78
sava <savamedvedev@gmail.com>
2022-11-04 21:45:05 +0800

    create basic functionality component TopPlay

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,29 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+/* eslint-disable */
+import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
+/////////////////////////////////////////////////////////////////////
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+/////////////////////////////////////////////////////////////////////
+
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+  return (
+    <>
+      <h1>TopPlay</h1>
+    </>
+  )
+};
 
 export default TopPlay;
